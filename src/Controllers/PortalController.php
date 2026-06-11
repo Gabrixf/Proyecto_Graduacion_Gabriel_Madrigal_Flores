f74f5047f5a89c72e3c71640fe1203f98b4e2a14
Gabriel Madrigal Flores <gmadrigalf101@gmail.com>
2026-06-11 15:56:31 -0600

    feat(portal): add PortalController

---
+++ src/Controllers/PortalController.php
@@ -0,0 +1,67 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controllers;
+
+use App\Services\PortalService;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use RuntimeException;
+use Slim\Routing\RouteContext;
+use Slim\Views\Twig;
+
+class PortalController
+{
+    public function __construct(
+        private readonly Twig          $twig,
+        private readonly PortalService $service
+    ) {}
+
+    public function perfil(Request $request, Response $response): Response
+    {
+        $datos = $this->service->perfil((int) $_SESSION['usuario_id']);
+        return $this->twig->render($response, 'portal/mi_perfil.html.twig', $datos + [
+            'titulo' => 'Mi Perfil',
+        ]);
+    }
+
+    public function colillas(Request $request, Response $response): Response
+    {
+        $datos = $this->service->colillas((int) $_SESSION['usuario_id']);
+        return $this->twig->render($response, 'portal/mis_colillas.html.twig', $datos + [
+            'titulo'     => 'Mis Colillas',
+            'flashError' => $this->consumeFlash('flash_error'),
+        ]);
+    }
+
+    public function colilla(Request $request, Response $response, array $args): Response
+    {
+        try {
+            $colilla = $this->service->colilla((int) $args['id'], (int) $_SESSION['usuario_id']);
+        } catch (RuntimeException) {
+            $_SESSION['flash_error'] = 'Colilla no encontrada.';
+            $url = RouteContext::fromRequest($request)->getRouteParser()->urlFor('portal.colillas');
+            return $response->withHeader('Location', $url)->withStatus(302);
+        }
+        return $this->twig->render($response, 'portal/colilla.html.twig', [
+            'titulo'  => 'Colilla de Pago',
+            'colilla' => $colilla,
+        ]);
+    }
+
+    public function vacaciones(Request $request, Response $response): Response
+    {
+        $datos = $this->service->vacaciones((int) $_SESSION['usuario_id']);
+        return $this->twig->render($response, 'portal/mis_vacaciones.html.twig', $datos + [
+            'titulo' => 'Mis Vacaciones',
+        ]);
+    }
+
+    private function consumeFlash(string $key): ?string
+    {
+        $msg = $_SESSION[$key] ?? null;
+        unset($_SESSION[$key]);
+        return $msg;
+    }
+}
